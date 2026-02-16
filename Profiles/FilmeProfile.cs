cf0910c7262d75404d75e017cd7da938647d898a
victorlesg <victorlesg.2004@gmail.com>
2026-02-16 10:16:06 -0300

    Filmes Api

---
+++ Profiles/FilmeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile()
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UptadeFilmeDto, Filme>();
+        CreateMap<Filme, UptadeFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+}
